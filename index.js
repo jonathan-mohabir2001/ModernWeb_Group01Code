9af6b3a359a14be0656989b0e6b3707242f886b5
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-29 17:49:58 -0500

    new commit, handlers added for routing.

--- index.js
+++ index.js
@@ -10,20 +10,21 @@ const PORT = process.env.PORT || 8000;
 const fs = require('fs')
 const path = require('path')
 
+const routes = require('./handlers/handlers')
+// routes contant responsible for routing. 
+// /handlers/handlers.js
+app.get('/', routes.home)
+app.get('/signup', routes.signUp)
+app.get('/login',routes.login)
+app.get('/addblog', routes.addBlog)
+// four routes to render our 4 pug pages. 
 
 
 
-
-
-
-
-
-
-app.set('view', path.join(__dirname,"views"))
 app.set('view engine', 'pug')
 // setting the view engine to pug. Allows for rendering of pug files. 
 
 app.listen(PORT, () => {
-  console.log('Server started press ctrl +c to end.')
+  console.log(`Server started press on port ${PORT}  press ctrl +c to end.`)
 })
 
